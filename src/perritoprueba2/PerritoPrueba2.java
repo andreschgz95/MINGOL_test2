e8e01c8b4f023ca85c1ff139a4bcf0dfc89932da
andreschgz95 <andreschgz@gmail.com>
2022-03-31 12:43:33 -0600

    31/mar/22 - Commit de andres

---
+++ src/perritoprueba2/PerritoPrueba2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package perritoprueba2;
+
+/**
+ *
+ * @author andre
+ */
+public class PerritoPrueba2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("esto es un ejemplo");
+    }
+    
+}
